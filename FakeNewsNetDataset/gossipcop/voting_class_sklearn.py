966219dd93b507c16b653eac4e658729099c9cc6
Stav Iatrop <staviatrop@gmail.com>
2020-01-25 16:55:33 +0200

    Updated voting classifier

--- FakeNewsNetDataset/gossipcop/voting_class_sklearn.py
+++ FakeNewsNetDataset/gossipcop/voting_class_sklearn.py
@@ -11,83 +11,115 @@ from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
 
 #Load train data
-X_train = pd.read_csv("train_gossipcop.csv", ",")
-Y_train = X_train['label'].values
-X_train = X_train['text'].values
+X_origin = pd.read_csv("train_gossipcop_vol2.csv", ",")
+Y_train = X_origin['label'].values
+X_origin = X_origin['text'].values
 print("Train set read.")
-#Load dev data
-# X_dev = pd.read_csv("dev_gossipcop.csv", ",")
-# Y_dev = X_dev['label'].values
-# X_dev = X_dev['text'].values
-# print("Dev set read.")
-X_test = pd.read_csv("test_gossipcop.csv", ",")
-Y_test = X_test['label'].values
-X_test = X_test['text'].values
+#Load test data
+X_test_origin = pd.read_csv("test_gossipcop_vol2.csv", ",")
+Y_test = X_test_origin['label'].values
+X_test_origin = X_test_origin['text'].values
 print("Test set read.")
 
 stopwords = set(ENGLISH_STOP_WORDS)
 
-vectorizer = TfidfVectorizer(sublinear_tf = True, max_df = 0.25, stop_words=stopwords)  #with 0.25 on average each of 3 classifiers has its best performance
-X_train = vectorizer.fit_transform(X_train)
-#X_dev = vectorizer.transform(X_dev)
-X_test = vectorizer.transform(X_test)
+print("Training SVM...")
+vectorizer = TfidfVectorizer(sublinear_tf = True, max_df = 0.25, stop_words=stopwords)
+X_train = vectorizer.fit_transform(X_origin)
+X_test = vectorizer.transform(X_test_origin)
 print("Vectorized.")
 
-svd = TruncatedSVD(n_components=1000, algorithm='arpack', random_state=42)
+svd = TruncatedSVD(n_components=150, algorithm='arpack', random_state=42)
+print("SVD prepared.")
 X_train = svd.fit_transform(X_train)
-#X_dev = svd.transform(X_dev)
 X_test = svd.transform(X_test)
+
 
 print("SVD finished.")
 
 
-svm = SVC(C=10,gamma=1, kernel='rbf', probability=True)
-LR = LogisticRegression(C=10, penalty='l2', solver='liblinear')
-#DT = DecisionTreeClassifier(criterion='gini', max_depth=6, min_samples_split=400)
-KNN = KNeighborsClassifier(metric='minkowski', n_neighbors=5, weights='distance', p = 6)
+svm = SVC(C=10, gamma='scale', kernel='rbf', random_state=42, probability=True)
 
-VC = VotingClassifier(estimators=[('svm', svm), ('LR', LR), ('KNN', KNN)], voting='soft')
+# svm.fit(X_train, Y_train)
+# Y_predict_svm = svm.predict(X_test)
+
+print("Training LR...")
+
+
+LR = LogisticRegression(C=10, penalty='l2', solver='liblinear')
+
+# LR.fit(X_train, Y_train)
+# Y_predict_LR = LR.predict(X_test)
+
+print("Training DT...")
+
+DT = DecisionTreeClassifier(criterion='entropy', max_depth=7, min_samples_split=420, random_state=42)
+
+# DT.fit(X_train, Y_train)
+# Y_predict_DT = DT.predict(X_test)
+
+print("Training KNN...")
+
+KNN = KNeighborsClassifier(n_neighbors=7, weights='distance', metric='euclidean')
+
+# KNN.fit(X_train, Y_train)
+# Y_predict_KNN = KNN.predict(X_test)
+
+print("Training RF...")
+
+RF = RandomForestClassifier(criterion='gini', max_depth=None, min_samples_split=2, n_estimators=180, random_state=42) 
+
+# RF.fit(X_train, Y_train)
+# Y_predict_RF = RF.predict(X_test)
+
+
+# sum = 0
+# for i in range(0, len(Y_test)):
+
+#     if (Y_predict_svm[i] == Y_predict_RF[i] and Y_predict_RF[i] == Y_predict_KNN[i] and Y_predict_KNN[i] == Y_predict_DT[i] and Y_predict_DT[i] == Y_predict_LR[i] ):
+#         sum = sum + 1
+
+# perc_agree = (sum / len(Y_test)) * 100
+# print( " Classifiers agree at about: " +  str(perc_agree))
+
+VC = VotingClassifier(estimators=[('svm', svm), ('LR', LR), ('KNN', KNN), ('DT', DT), ('RF', RF)], voting='soft')
 VC = VC.fit(X_train, Y_train)
 print("Trained.")
-
-Y_predict_train = VC._predict(X_train)
-print("Train predicted.")
-
-Y_predict_train_final = VC.predict(X_train)
-mislabel = 0
-_all = len(Y_predict_train)
-for i in range(len(Y_predict_train)):
-
-    if Y_predict_train_final[i] != Y_train[i]:
-        if Y_train[i] in Y_predict_train[i]:    
-            mislabel = mislabel + 1 
-
-percentage = mislabel / _all
-print("Percentage of mislabeled samples that one or more classifier had predicted right on train set:" + 
-        str(percentage))
 
 Y_predict_test = VC._predict(X_test)
 print("test predicted.")
 
 Y_predict_test_final = VC.predict(X_test)
-mislabel = 0
-_all = len(Y_predict_test)
-for i in range(len(Y_predict_test)):
+# mislabel = 0
+# _all = len(Y_predict_test)
+# for i in range(len(Y_predict_test)):
 
     
-    if Y_predict_test_final[i] != Y_test[i]:
-        if Y_test[i] in Y_predict_test[i]:
-            mislabel = mislabel + 1 
+#     if Y_predict_test_final[i] != Y_test[i]:
+#         if Y_test[i] in Y_predict_test[i]:
+#             mislabel = mislabel + 1 
 
-percentage = mislabel / _all
-print("Percentage of mislabeled samples that one or more classifier had predicted right on test set:" + 
-        str(percentage))
-
-print("Train accuracy: " + str(accuracy_score(Y_train, Y_predict_train_final)))
-print("Train F1 score: " + str(f1_score(Y_train, Y_predict_train_final)))
+# percentage = mislabel / _all
+# print("Percentage of mislabeled samples that one or more classifier had predicted right on test set:" + 
+#         str(percentage))
 
 print("test accuracy: " + str(accuracy_score(Y_test, Y_predict_test_final)))
 print("test F1 score: " + str(f1_score(Y_test, Y_predict_test_final)))
+
+cf_matrix = confusion_matrix(Y_test, Y_predict_test_final, labels=[0, 1])
+htmp_test = sns.heatmap(cf_matrix, cmap='Reds', annot=True, fmt='g')
+plt.title("Voting Classifier: Confusion Matrix of Test set")
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+
+plt.show()
+
+plt.clf()
